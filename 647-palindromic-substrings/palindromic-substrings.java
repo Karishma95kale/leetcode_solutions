f2d51fef9b1f5d63fc77720697eeeef08289ccfe
Karishma95kale <karishmakale9595@gmail.com>
2026-08-10 22:20:37 +0530

    Time: 5 ms (98.15%) | Memory: 43 MB (55.54%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public static int expand(String s, int left, int right) {
+
+        int count = 0;
+
+        while (left >= 0 &&
+               right < s.length() &&
+               s.charAt(left) == s.charAt(right)) {
+
+            count++;
+
+            left--;
+            right++;
+        }
+
+        return count;
+    }
+
+    public int countSubstrings(String s) {
+
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int odd = expand(s, i, i);
+            int even = expand(s, i, i + 1);
+
+            count += odd + even;
+        }
+
+        return count;
+    }
+}
